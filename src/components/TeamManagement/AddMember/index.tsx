77c5092fe9eb468e40bc0ccb86f83cc5b8387928
Mohammad <mohammadyasrebi@gmail.com>
2022-05-27 19:16:43 +0200

    Fixing some styling issues

--- src/components/TeamManagement/AddMember/index.tsx
+++ src/components/TeamManagement/AddMember/index.tsx
@@ -13,6 +13,15 @@ import members from "../../../assets/data.json"
 const AddMember = () => {
   const dispatch = useDispatch();
   const [showMembersList, setShowMembersList] = useState(false);
+  const [hovered, setHovered] = useState(false);
+
+  const mouseOverHandler = () => {
+    setHovered(true);
+  }
+
+  const mouseOutHandler = () => {
+    setHovered(false);
+  }
 
   const memberSelectedHandler = (selectedOption: any): void => {
     dispatch(memberActions.add(selectedOption));
@@ -28,9 +37,9 @@ const AddMember = () => {
 
   return (
     <Card>
-      {!showMembersList && <div className="team-member">
+      {!showMembersList && <div className="team-member" onMouseOver={mouseOverHandler} onMouseOut={mouseOutHandler}>
         <div className="team-member__avatar">
-          <Icon className="team-member__avatar--add" onClick={addClickHandler}><span className="material-icons">add</span></Icon>
+          <Icon className={`team-member__avatar--add ${hovered && "hovered"}`} onClick={addClickHandler}><span className="material-icons">add</span></Icon>
         </div>
         <div className="team-member__description">
           <span className="team-member__description--summary add">Add team member to this test</span>
